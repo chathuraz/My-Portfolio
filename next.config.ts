7632c17e05e6daafa83eb7d49b2c17bd41d9b286
Chathura Sajeewa <sajeewachathura2001@outlook.com>
2025-11-09 11:47:58 +0530

    fix minor changes

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/My-Portfolio",
+  assetPrefix: "/My-Portfolio",
+};
+
+export default nextConfig;
